22ed738d11a6904f214136c4fb0c3b6de81b03a2
MUHAMMAD MOHZARY <mmohzary@kent.edu>
2020-04-05 21:37:37 -0500

    m2icp1 is done

--- M2ICP1/source/task2/SecondarySorting/src/main/scala/SecondarySorting.scala
+++ M2ICP1/source/task2/SecondarySorting/src/main/scala/SecondarySorting.scala
@@ -14,15 +14,26 @@ object SecondarySorting {
     //To create RDD from input dataset file using textfile method
     val RDD = sc.textFile("input/dataset.txt")
 
+
+    //To create a new pair RDD from the existing RDD we used map() and split() methods forst to split data using ","
+    // then we map output of split method into key value pairs
     val pairRDD =RDD.map(_.split(",")).map{k => (k(0),k(1))}
+
+    //to define the number of partitions we want in the reducing function
     val numReducers = 2;
+
+    //To group pairs RDD by key and sort them using value instead of using key
     val listRDD = pairRDD.groupByKey(numReducers).mapValues(iter => iter.toList.sortBy(r => r))
+
+    //To use flat map to return a list of multiples values for each element in the original RDD
     val resultRDD =listRDD.flatMap{
       case (labe1,list) => {
 
         list.map((labe1, _))
       }
     }
+
+    //to save the output into text file in the output folder
     resultRDD.saveAsTextFile("output/")
   }
 }
