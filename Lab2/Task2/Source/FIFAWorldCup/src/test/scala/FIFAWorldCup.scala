64bee43aa0eda59326e6d6044c4ecc73e86ba203
mohzary <mmohzary@kent.edu>
2020-04-28 07:00:31 -0500

    Task 2 code

--- Lab2/Task2/Source/FIFAWorldCup/src/test/scala/FIFAWorldCup.scala
+++ Lab2/Task2/Source/FIFAWorldCup/src/test/scala/FIFAWorldCup.scala
@@ -104,7 +104,7 @@ object FIFAWorldCup {
     val query8 = spark.sql("SELECT CITY, Stage, COUNT(City) AS Total FROM matches WHERE Stage=='Final' GROUP BY City, Stage ORDER BY Total DESC")
     println("Total of how many times city hosted final match")
     query8.show()
-    
+
     //Query(9) To find out list of stadium that hosted final matches
     val query9 = spark.sql("SELECT Stadium, City, Stage FROM matches where Stage=='Final' GROUP BY City, Stadium, Stage")
     println("List of stadium hosted final matches in the world cup")
@@ -114,6 +114,52 @@ object FIFAWorldCup {
     val query10 = spark.sql("SELECT Datetime, Stage, Stadium, City, HomeTeamName, HomeTeamGoals, AwayTeamGoals, AwayTeamName FROM matches WHERE Stage=='Final' GROUP BY Stage, Stadium, City, HomeTeamName, HomeTeamGoals, AwayTeamGoals, AwayTeamName, Datetime")
     println("List of Final Matches:")
     query10.show()
+
+    //-------------------------Part(3): Perform any 5 queries in Spark RDD’s and Spark Data Frames-------------------
+    // To create a new RDD from worldcups dataset
+    val RDD = sc.textFile("input/WorldCups.csv")
+    val header = RDD.first()
+    val world_cups_RDD = RDD.filter(line => line != header)
+
+    // Query(1): To find out Country hosted World Cup in 2014 using RDD
+    val query1_rdd = world_cups_RDD.filter(line => line.split(",")(0) == "2014").map(line => (line.split(",")(0), (line.split(",")(1))) )
+    println("Country hosted World Cup in 2014 is: (RDD Result)\n")
+    query1_rdd.foreach(println)
+
+    println("\nCountry hosted World Cup in 2014 is: (Data Frame Result)\n")
+    world_Cups_DF_noDUP.select("Year", "Country").filter("Year = 2014").show()
+
+    //Query(2): To find a list of winner teams and year
+    val query2_rdd = world_cups_RDD.map(line => (line.split(",")(0), (line.split(",")(2))) )
+    println("list of winner teams and year: (RDD Result)\n")
+    query2_rdd.foreach(println)
+
+    println("\nlist of winner teams and year: (Data Frame Result)\n")
+    world_Cups_DF_noDUP.select("Year", "Winner").show()
+
+    // Query(3): To find out list of World Cups where GoalsScored is greater than 150
+    val query3_rdd = world_cups_RDD.filter(line => line.split(",")(6) > "150").map(line => (line.split(",")(0), line.split(",")(6), (line.split(",")(1))) )
+    println("list of World Cups where GoalsScored is greater than 150: (RDD Result)\n")
+    query3_rdd.foreach(println)
+
+    println("\nlist of World Cups where GoalsScored is greater than 150: (Data Frame Result)\n")
+    world_Cups_DF_noDUP.select("Year", "GoalsScored", "Country").filter("GoalsScored > 150").show()
+
+    // Query(4): To find out list of World Cups where Winner is Brazil and the Runners-up is Germany
+    val query4_rdd = world_cups_RDD.filter(line => line.split(",")(2) == "Brazil" && line.split(",")(3) == "Germany").map(line => (line.split(",")(0), line.split(",")(2), (line.split(",")(3))) )
+    println("list of World Cups where Winner is Brazil and the Runners-up is Germany: (RDD Result)\n")
+    query4_rdd.foreach(println)
+
+    println("\nlist of World Cups where Winner is Brazil and the Runners-up is Germany: (Data Frame Result)\n")
+    world_Cups_DF_noDUP.select("Year", "GoalsScored", "Runners-Up").filter("Winner=='Brazil' AND 'Runners-Up'=='Germany'").show()
+
+    //Query(5) To show how many matches had Argentina played and won
+    val query5_rdd = world_cups_RDD.filter(line => (line.split(",")(2)=="Argentina" ))
+      .map(line => (line.split(",")(0), line.split(",")(1), line.split(",")(2), line.split(",")(3), line.split(",")(4), line.split(",")(5), line.split(",")(6), line.split(",")(7), line.split(",")(8), line.split(",")(9))).collect()
+    println("To show how many matches had Argentina played and won: (RDD Result)\n")
+    query5_rdd.foreach(println)
+    println("\nTo show how many matches had Argentina played and won: (Data Frame Result)\n")
+    world_Cups_DF_noDUP.select("*").filter("Country == 'Argentina'").show(2)
 
   }
 }
